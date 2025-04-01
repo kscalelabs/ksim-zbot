171c535e0e61fa87e1cea9b3e24997219b002d4f
alik-git <ali@kscale.dev>
2025-03-31 23:31:12 -0700

    update height in healthy rewward

--- ksim_zbot/zbot2/standing.py
+++ ksim_zbot/zbot2/standing.py
@@ -263,8 +263,8 @@ class DHControlPenalty(ksim.Reward):
 class DHHealthyReward(ksim.Reward):
     """Legacy default humanoid healthy reward that gives binary reward based on height."""
 
-    healthy_z_lower: float = attrs.field(default=0.5)
-    healthy_z_upper: float = attrs.field(default=1.5)
+    healthy_z_lower: float = attrs.field(default=0.25)
+    healthy_z_upper: float = attrs.field(default=0.6)
 
     def __call__(self, trajectory: ksim.Trajectory) -> Array:
         height = trajectory.qpos[..., 2]
